9f3493e1532d32c9240e79af172b884d0ba967a5
Lucaswillians <lucaswilianserpa@gmail.com>
2025-11-16 21:10:55 -0300

    chore: pipeline test config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,14 +1,31 @@
+/** @type {import('jest').Config} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
+
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   setupFiles: ["<rootDir>/jest.setup.ts"],
+
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+
   globals: {
     "ts-jest": {
-      tsconfig: "tsconfig.test.json"
-    }
-  }
+      tsconfig: "tsconfig.test.json",
+    },
+  },
+
+  // >>> ADICIONADO <<<
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx,js,jsx}",
+    "!src/main.tsx",
+    "!src/vite-env.d.ts",
+    "!src/**/__tests__/**",
+    "!src/**/mocks/**",
+    "!src/**/types/**",
+  ],
 };
